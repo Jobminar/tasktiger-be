19c842a575b79aefcc1b20b656bf4b72d09af76e
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2024-09-25 16:21:42 +0530

    added the serviceAccountKey.js

---
+++ api/v1.0/modules/_core/notifications/serviceAccountKey.js
@@ -0,0 +1,17 @@
+const serviceAccount = {
+  type: "service_account",
+  project_id: "coolie-9cc38",
+  private_key_id: "4277651f9363e92e3622dee78650cb51e8c48f4e",
+  private_key:
+    "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCaOShbwitblFZu\nL9kysn/AbdCsuco3dYm9myraIOgEI/KbXaZ4gAqUytUM+TxYZkxvX6OhWO8Tls4n\nN25K20PDZ5RcVaDPsp4DzI//ULsoxCBhxQDa52IPosuUd+vnmMJzLXNkjBIsxWvo\nedX+ecKB1/OLBjF7Whw/bqNJVigk4FY/wvAB6jcfKNRuzWHKqf1f2cBevPTV1C49\nzZ0DRbDXXqilMm5LQ5oOMysKTludsDm8tHI17Gyt78hjmXnE4hUr0EyEetZinMdg\n5DljqrZ3m6maLLy3jDyfL038PhswPhj0CmYuZ7h0sh9llwojBJ63SRzydvwwWKRz\nAuAP4GgnAgMBAAECggEANS7QxBPJb+NPit/lPp5ff7Re6bGIEmft//yqYl5GpGbh\nGsLIzXUaMQVhQwAP10wvv5hzjQhmKZMvtEZhOl8ylwf5GX5N3lHyO80nOxxT7lLz\n98M/8D0qi8nP+nsAVvVO2Pkredjzy4ypEZoyOXczpCMcUSzpk2gXpag5luinH0/U\nP4+J9pnZpHqVpAQQQuWx/WSR+hm45ZM/9HRQOXbPaUe/HORpgGncYG/u8qhn8UUG\nTqxJNKUKArpHOnbb6eWVKhVzE8p1vTvF1YV4gJhAZgTG+x485cajcY5hFLN4oVZW\njzJBLpuE4JCaMhNu7ugx+9TQELbmUgxixRtTUKrgwQKBgQDP6dGJ8QuHwrL9gwRk\neN4ZdEb6qT5fJWlOhLGGR24bDgUlGLp0kJsBvHeqAGrlKWbSnL2yDc6Tw6M+LI2x\nK60rKChezlZd3MUoof6CP7op8vB1eg15isxcCNOKjZ65DYCiRdMbuf37y8nJQqVM\n517T1NlMvTlVN4bRxeeOy4KaYwKBgQC95HHKCk1UIFTrsFHGx0MuSKfkznaquIa4\n2cZmgQqA2jbkjLNZdZGF/PRUVXj0k9tOEm6kPEEF3XkaPwrOHiDl2X0B0KGZVJ5n\nizTwbw39LYZbAJ44WHqKIlhKPS70boJ4s7wLSH/9aA11jRYoIzCzux8eKzGcW9YH\nCY9lbhJkbQKBgQCBcu8Hl5C6Qjr3JG/Dro16A699uYof60cZn/x3wlmHdr81TaQp\nly8rcZB/xCrJ63EdiIGDLp2D2rb0qwC9Ypg62pHoFUgowJ/woIu5xLvACoY0Xn2t\nQwHh+4zaIzrDtOh2IEtEZBUChOtp55jLvPKJqWhyIw5jRGMokLOPcJ4sSQKBgQCg\nwhc7DkGs6Lo71cdFW0galNPeNpPrr9AOCf+Wo8P6lcJG9n6XS5rpLd7CzqFZnRra\nG2yjiYcaqgZuG9TP7l23eMoFkVRddG+uBQHKUporLjFCNWG9HdqPuse4AdgQgsXd\n3XtiqfwA0egjDlAlprUCU3XimnqjfnPQ/VzyZ58WeQKBgAod//vxtMKQV5v/e0HN\nHclCWUicrtRZ5kQUakck8pg4q9G52ON27UEMeHNffbmlVBFRwgRQMglwGj+kMJfP\nBI0TJZMZviLAmUvClu1P22+aLZNWEqGxPCOJCGOvleqei1a5IatEVaY1W5zg+4sh\ntQq0azdp0GTCdIMVIGlc4SAc\n-----END PRIVATE KEY-----\n",
+  client_email: "firebase-adminsdk-sx4m9@coolie-9cc38.iam.gserviceaccount.com",
+  client_id: "110897243264146710445",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-sx4m9%40coolie-9cc38.iam.gserviceaccount.com",
+  universe_domain: "googleapis.com",
+};
+
+export default serviceAccount;
